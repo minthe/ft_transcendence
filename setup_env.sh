a1d2c42a1cc8f4eb58873702312203597171a817
vfuhlenb <vfuhlenb@students.42wolfsburg.de>
2024-02-29 18:38:05 +0100

    added creation of selfsigned certs

--- setup_env.sh
+++ setup_env.sh
@@ -11,3 +11,9 @@ if grep -q '^CURRENT_HOST=' .env; then
 else
     echo "CURRENT_HOST='$current_host'" >> .env
 fi
+
+if [[ $current_host == *"42wolfsburg"* || $current_host == *"localhost"* || $current_host == *"127.0."* || $current_host == *"192.168."* ]]; then
+    openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+        -keyout "./.ssl/privkey_$current_host.pem" -out "./.ssl/fullchain_$current_host.pem" \
+        -subj "/CN=$current_host"
+fi
