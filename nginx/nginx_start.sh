ad9960cbdfa3347e82bd7a93941fbd845e96f304
vfuhlenb <minh.tee@gmail.com>
2024-02-29 22:59:01 +0100

    localhost and hostname deployment

--- nginx/nginx_start.sh
+++ nginx/nginx_start.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-# Substitute environment variables in nginx.conf.template and create nginx.conf
 envsubst '$CURRENT_HOST' < /nginx.conf.template > /etc/nginx/nginx.conf
 nginx -g 'daemon off;'
---
+++ setup_cert.sh
@@ -0,0 +1,17 @@
+#!bin/bash
+
+current_host=$(hostname)
+
+if [ ! -d "./.cert" ]; then
+    mkdir .cert
+fi
+
+if grep -q "^CURRENT_HOST=" .env; then
+    if grep -q "^CURRENT_HOST='localhost'" .env || [[ $current_host == *"42wolfsburg"* ]]; then
+        openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+            -keyout "./.cert/privkey_localhost.pem" -out "./.cert/fullchain_localhost.pem" \
+            -subj "/CN=$current_host"
+    fi
+else
+    echo "CURRENT_HOST='localhost'" >> .env
+fi
