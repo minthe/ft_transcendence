7a6ce819e26f6ba19b3420139130b35377739d5b
vfuhlenb <minh.tee@gmail.com>
2024-03-22 18:31:59 +0100

    updated view for oauth redirect

--- auth/django_project/urls.py
+++ auth/django_project/urls.py
@@ -4,5 +4,5 @@ from intralogin import views
 urlpatterns = [
     path('auth/user', views.get_authenticated_user, name='get_authenticated_user'),
 	path('auth/login', views.intra_login, name='auth_login'),
-	path('auth/login/redirect', views.intra_login_redirect, name='auth_login_redirect')
+	path('oauth/login/redirect', views.intra_login_redirect, name='auth_login_redirect')
 ]
