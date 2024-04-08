5004a51a4bf41f9215e18aaf5471e31d63e6da25
vfuhlenb <vfuhlenb@students.42wolfsburg.de>
2024-04-08 13:44:51 +0200

    [game_chat] django secret key fetched from .env

--- ms_game_chat/backend/backend/settings.py
+++ ms_game_chat/backend/backend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!l1#n)(q==6k0w3#*w92ilmkltpzf9d1ep4*39yiqz*$$z%ve^'
+SECRET_KEY = os.environ.get('DJANGO_SECRET')
 
 JWT_SECRET = os.environ.get('JWT_SECRET')
 
