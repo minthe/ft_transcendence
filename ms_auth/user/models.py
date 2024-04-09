e7e20e1b8d9da8304d888fb11b2a6a74705c37ee
vfuhlenb <vfuhlenb@students.42wolfsburg.de>
2024-04-09 19:11:55 +0200

    [auth] updated REST API spec to 0.3; changed user_id to userId

--- ms_auth/user/models.py
+++ ms_auth/user/models.py
@@ -2,15 +2,15 @@ from django.db import models
 from django.contrib.auth.hashers import make_password, check_password
 
 class User(models.Model):
-	sub = models.BigIntegerField(primary_key=True)
-	intra_id = models.CharField(max_length=12, null=True)
+	userId = models.BigIntegerField(primary_key=True)
+	two_factor_enabled = models.BooleanField(null=True)
+	intra_id = models.CharField(max_length=12, unique=True, null=True)
 	username = models.CharField(max_length=255, unique=True, null=True)
 	password = models.CharField(max_length=255, null=True)
+	avatar = models.CharField(max_length=2000, null=True)
 	email = models.CharField(max_length=255, unique=True, null=True)
-	image = models.CharField(max_length=255, null=True)
-	jwt = models.CharField(max_length=255, null=True)
 
-	sub = models.AutoField(primary_key=True)
+	userId = models.AutoField(primary_key=True)
 
 	def set_password(self, plain_password):
 		self.password = make_password(plain_password)
