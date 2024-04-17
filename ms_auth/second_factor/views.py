bc5be64b252885676831ec1066f074997047583e
vfuhlenb <vfuhlenb@students.42wolfsburg.de>
2024-04-17 16:13:25 +0200

    removed the max tries constraint

--- ms_auth/second_factor/views.py
+++ ms_auth/second_factor/views.py
@@ -1,19 +1,14 @@
-from django.conf import settings
 from django.utils.crypto import get_random_string
 from django.utils import timezone
 from django.contrib.auth.hashers import make_password, check_password
 from user import views as user_views
-from ft_jwt.ft_jwt.ft_jwt import FT_JWT
-
-jwt = FT_JWT(settings.JWT_SECRET)
 
 def generate_second_factor_dict():
 	code = get_random_string(length=6, allowed_chars='0123456789')
 	timestamp = timezone.now().isoformat()
 	second_factor_dict = {'code': make_password(code),
 						  'timestamp': timestamp,
-						  'last_retry': timestamp,
-						  'second_factor_retry_count': 0}
+						  'last_retry': timestamp}
 	return second_factor_dict
 
 def verify_verification_code(user_id, submitted_code):
@@ -29,10 +24,6 @@ def verify_verification_code(user_id, submitted_code):
 		second_factor_dict['second_factor_retry_count'] = second_factor_retry_count + 1
 		user_views.updateValue(user_id, 'second_factor_dict', second_factor_dict)
 		return False, 'Rate limit exceeded'
-
-	if second_factor_retry_count >= 3:
-		user_views.updateValue(user_id, 'second_factor_dict', second_factor_dict)
-		return False, 'Maximum retries of 3 reached'
 
 	if not check_password(submitted_code, code):
 		second_factor_dict['last_retry'] = current_time.isoformat()
