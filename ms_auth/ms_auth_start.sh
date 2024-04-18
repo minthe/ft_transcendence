b04d7deed5b9293351183f24b12935b230d49391
vfuhlenb <vfuhlenb@students.42wolfsburg.de>
2024-04-18 18:34:20 +0200

    flag for debug mode in .env

--- ms_auth/ms_auth_start.sh
+++ ms_auth/ms_auth_start.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-if $DEBUG
-then
-	echo "Debug mode is on"
+echo "DEBUG is set to: $DEBUG"
+
+if [ "$DEBUG" = "true" ]; then
+    echo "Debug mode is on"
 	echo "Starting with django runserver"
 	WEBSERVER='python manage.py runserver 0.0.0.0:8000'
-
 else
-	echo "Debug mode is off"
+    echo "Debug mode is off"
 	echo "Starting with daphne"
 	WEBSERVER='daphne -b 0.0.0.0 -p 8000 django_project.asgi:application'
 fi
--- ms_game_chat/backend/ms_game_chat_start.sh
+++ ms_game_chat/backend/ms_game_chat_start.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-if $DEBUG
-then
-	echo "Debug mode is on"
+echo "DEBUG is set to: $DEBUG"
+
+if [ "$DEBUG" = "true" ]; then
+    echo "Debug mode is on"
 	echo "Starting with django runserver"
 	WEBSERVER='python manage.py runserver 0.0.0.0:6969'
-
 else
-	echo "Debug mode is off"
+    echo "Debug mode is off"
 	echo "Starting with daphne"
 	WEBSERVER='daphne -b 0.0.0.0 -p 6969 backend.asgi:application'
 fi
