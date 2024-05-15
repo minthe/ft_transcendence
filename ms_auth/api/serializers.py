723044f660db0a04fc4e10e23e7026a935e50f57
minthe <minh.tee@gmail.com>
2024-05-15 08:17:56 +0200

    updated input validation

--- ms_auth/api/serializers.py
+++ ms_auth/api/serializers.py
@@ -4,23 +4,23 @@ from django.core.exceptions import ValidationError
 
 def validate_username(username):
 	# TODO valentin add check if username already exists from 42 Intra
+	if len(username) < 1:
+		raise ValidationError("Username cannot be empty")
 	if not username.isalnum():
 		raise ValidationError("Username must be alphanumeric")
-	if len(username) < 3:
-		raise ValidationError("Username must be at least 3 characters long")
 	return username
 
 def validate_alias(alias):
 	# TODO valentin add check if alias already exists from 42 Intra
+	if len(alias) < 1:
+		raise ValidationError("alias cannot be empty")
 	if not alias.isalnum():
 		raise ValidationError("alias must be alphanumeric")
-	if len(alias) < 1:
-		raise ValidationError("alias must be at least 1 characters long")
 	return None
 
 
 def validate_password(password):
-	pattern = r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&,])[A-Za-z\d@$!%*?&,]{8,}$'
+	pattern = r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&,;:{}])[A-Za-z\d@$!%*?&,;:{}]{8,}$'
 	validator = RegexValidator(regex=pattern, message='Password must contain at least 1 uppercase letter, 1 lowercase letter, 1 digit, 1 of these special characters @$!%*?&, and be at least 8 characters long')
 	try:
 		validator(password)
