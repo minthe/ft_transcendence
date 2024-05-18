8ee0b843b91f7eedf81f0a3d8ba723dc1cf6a0bb
NULL-Term1nat0r <eike.struckmeier@web.de>
2024-05-18 18:41:40 +0200

    fixed entrypoint script

--- postgres/docker-ensure-initdb.sh
+++ postgres/docker-ensure-initdb.sh
@@ -1,4 +1,22 @@
 #!/usr/bin/env bash
+
+export VAULT_TOKEN="root"
+export VAULT_ADDR="http://vault:8200"
+
+postgres_user=$(vault read -format=json secret/data/POSTGRES_USER | jq -r '.data.data.POSTGRES_USER')
+# postgres_user=$(echo "$postgres_user" | awk '{$1=$1};1')
+postgres_password=$(vault read -format=json secret/data/POSTGRES_PASSWORD | jq -r '.data.data.POSTGRES_PASSWORD')
+# postgres_password=$(echo "$postgres_passwword" | awk '{$1=$1};1')
+postgres_port=$(vault read -format=json secret/data/POSTGRES_PORT | jq -r '.data.data.POSTGRES_PORT')
+# postgres_port=$(echo "$postgres_port" | awk '{$1=$1};1')
+postgres_db=$(vault read -format=json secret/data/POSTGRES_DB | jq -r '.data.data.POSTGRES_DB')
+# postgres_db=$(echo "$postgres_db" | awk '{$1=$1};1')
+
+export POSTGRES_USER="$postgres_user"
+export POSTGRES_PASSWORD="$postgres_password"
+export POSTGRES_PORT="$postgres_port"
+export POSTGRES_DB="$postgres_db"
+
 set -Eeuo pipefail
 
 #
--- postgres/set_env.sh
+++ postgres/set_env.sh
@@ -6,7 +6,6 @@ export VAULT_TOKEN="root"
 export VAULT_ADDR="http://vault:8200"
 
 postgres_user=$(vault read -format=json secret/data/POSTGRES_USER | jq -r '.data.data.POSTGRES_USER')
-
 # postgres_user=$(echo "$postgres_user" | awk '{$1=$1};1')
 postgres_password=$(vault read -format=json secret/data/POSTGRES_PASSWORD | jq -r '.data.data.POSTGRES_PASSWORD')
 # postgres_password=$(echo "$postgres_passwword" | awk '{$1=$1};1')
@@ -14,14 +13,6 @@ postgres_port=$(vault read -format=json secret/data/POSTGRES_PORT | jq -r '.data
 # postgres_port=$(echo "$postgres_port" | awk '{$1=$1};1')
 postgres_db=$(vault read -format=json secret/data/POSTGRES_DB | jq -r '.data.data.POSTGRES_DB')
 # postgres_db=$(echo "$postgres_db" | awk '{$1=$1};1')
-
-echo "Retrieved secrets from Vault:"
-# echo "$(vault read -format=json secret/data/POSTGRES_USER | jq -r '.data.data.POSTGRES_USER')"
-# echo "test"
-echo "POSTGRES_USER: $postgres_user"
-echo "POSTGRES_PASSWORD: $postgres_password"
-echo "POSTGRES_PORT: $postgres_port"
-echo "POSTGRES_DB: $postgres_db"
 
 export POSTGRES_USER="$postgres_user"
 export POSTGRES_PASSWORD="$postgres_password"
